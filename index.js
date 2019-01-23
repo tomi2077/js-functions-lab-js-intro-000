328f947a16aea3a5569074a1e1939b9226ad0b19
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 12:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-happyHolidayTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-holidayCountdown(holiday, days){
+function holidayCountdown(holiday, days){
   return `It\'s ${days} days until ${holiday}!`
 }
